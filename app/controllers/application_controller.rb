d414d27e70581874caac3fa00bc3931f48b39696
vovan4eg <vovan4eg@ua.fm>
2014-08-02 11:48:36 +0300

    Added test for user first name, last name and profile correctness.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
+
+
   protected
 
   def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << :first_name << :last_name << :profile_name
-      devise_parameter_sanitizer.for(:account_update) << :first_name << :last_name << :profile_name
+      devise_parameter_sanitizer.for(:sign_up) do |u|
+          u.permit(:email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name, :user_id)
+      end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true,
+            uniqueness: true,
+            format: {
+                with: /\A[a-zA-Z\-\_]+\Z/,
+                message: "Must be formatted correctly."
+            }
+
   has_many :statuses
 
 
