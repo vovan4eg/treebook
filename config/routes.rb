b2e196191231cc875d1a791eec62633da51ba495
vovan4eg <vovan4eg@ua.fm>
2014-08-02 14:15:54 +0300

    Added custon routes for auth and feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :statuses
 
+  devise_scope :user do
+      get "register", :to => "devise/registrations#new", as: :register
+      get "login", :to => "devise/sessions#new", as: :login
+      get "logout", :to => "devise/sessions#destroy", as: :logout
+
+
+  end
+
+  resources :statuses
+  
   root to: "statuses#index"
 
 
